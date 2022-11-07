80fd65cde05792adac0ac08535e0cc4530825ec0
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-07 21:14:35 +0200

    v 1.1

--- Unlimited_Real.cpp
+++ Unlimited_Real.cpp
@@ -1,7 +1,13 @@
 // #include "BigRealClass.h"
 #include "BigRealClass.cpp"
+#include "BigDecimalIntClass.cpp"
 
 int main()
 {
-    BigReal o1("+5758.5208");
+    BigDecimalInt o3("65");
+    string s1 = "2568.55";
+    BigReal o1(s1), o2("+558.70");
+    // BigReal o5(o3);
+    o1.assNumber("-555.788");
+
 }
