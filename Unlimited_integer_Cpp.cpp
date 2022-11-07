78f301749f49062eee8ff4baeb819ba81b235217
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-07 21:15:54 +0200

    v1.1

--- Unlimited_integer_Cpp.cpp
+++ Unlimited_integer_Cpp.cpp
@@ -1,56 +1,143 @@
-#include "BigDecimalInt.cpp"
+// FCAI – Object-Oriented Programming 1 – 2022 - Assignment 1
+// Program Name: BigDecimalInt
+// Last Modification Date: 13/10/2022
+// Author1 and ID and Group: Basmala Mohamed Sayed Gad  ID: 20210090 (a, b, c)
+// Author2 and ID and Group: Aya Ali Hassan  ID: 20210083 (d, e)
+// Author3 and ID and Group: Mohamed Ashraf Fahim  ID: 20210329 (f, i, j , k)
+/*
+description: In this problem we developed a new C++ type (class).
+that can hold unlimited decimal integer values and performs arithmetic operations on them.
+such as: +, -, <, and >.
+ */
 
+#include "BigDecimalIntClass.cpp"
 
-int main()
-{
-    BigDecimalInt("+ 00099999");
-    BigDecimalInt o1("+500");
-    BigDecimalInt o2("+111111111111111111000000000000000000000001111111111");
-    BigDecimalInt o3("+1111004548");
-    BigDecimalInt o4 = o3;
-    BigDecimalInt o5 ("-121");
-    BigDecimalInt o6 ("-11111");
-    BigDecimalInt o7 ("-124");
-    BigDecimalInt o8 ("+200");
-    BigDecimalInt o9 ("+12550014780014");
-    BigDecimalInt o10 ("+12550014780014");
-    BigDecimalInt o13 ("-12550014780014");
-    BigDecimalInt o11 ("+55654354499565");
-    BigDecimalInt o12 ("-2842695969599900001111");
-    
+int main() {
+/*    string n1 = "-34", n2 = "100", n3 = "-500", n4 = "7";
 
-    cout << char(o1.Sign()) << endl;
-    cout << o1.Size() << endl;
+    BigDecimalInt input(n1), input3(n2);
+    BigDecimalInt e = input + input3;
+    BigDecimalInt d = input - input3;
+    cout << "---------TEST ONE---------" << "\n";
+    cout << e;
+    cout << d;
+    cout << (input < input3) << "\n";
+    cout << (input > input3) << "\n";
+    cout << input.Sign() << "\n";
+    cout << input3.size() << "\n";
 
-    BigDecimalInt a;
-    cout << "o9 + o10: " ; a = o9 + o10 ;
-    cout << "o13 + o9: " ; a = o13 + o9 ;
-    cout << "o6 + o10: " ; a = o6 + o10;
-    cout<<endl;
-    cout << "o7 - o5: "; a = (o7 - o5);
-    cout << "o5 - o7: "; a = (o5 - o7);     
-    cout << "o8 - o7: "; a = (o8 - o7);
-    cout << "o7 - o7: "; a = (o7 - o7);
-    cout << "o7 - o8: "; a = (o7 - o8);
-    cout << "o1 - o8: "; a = (o1 - o8);
-    cout << "o8 - o1: "; a = (o8 - o1);
+    BigDecimalInt in(n1), in2(n3);
+    BigDecimalInt a = in + in2;
+    BigDecimalInt b = in - in2;
+    cout << "---------TEST TWO---------" << "\n";
+    cout << a;
+    cout << b;
+    cout << (in < in2) << "\n";
+    cout << (in > in2) << "\n";
+    cout << in2.Sign() << "\n";
+    cout << in.size() << "\n";
 
-    cout<<endl;
+    BigDecimalInt input6(n2), input7(n3);
+    BigDecimalInt h = input6 + input7;
+    BigDecimalInt f = input6 - input7;
+    cout << "---------TEST THREE---------" << "\n";
+    cout << h;
+    cout << f;
+    cout << (input6 < input7) << "\n";
+    cout << (input6 > input7) << "\n";
+    cout << input6.Sign() << "\n";
+    cout << input7.size() << "\n";
 
-    
-    cout <<"o3 < o2: "<< (o3 < o2 )<< endl;
-    cout <<"o3 < o4: "<< (o3 < o4)<< endl;
-    cout << "o11 < o12: "<< (o11 < o12)<<endl;
- 
-    cout<<endl;
+    BigDecimalInt in1(n4), in3(n2);
+    BigDecimalInt k = in1 + in3;
+    BigDecimalInt l = in1 - in3;
+    cout << "---------TEST FOUR---------" << "\n";
+    cout << k;
+    cout << l;
+    cout << (in1 < in3) << "\n";
+    cout << (in1 > in3) << "\n";
+    cout << in1.Sign() << "\n";
+    cout << in3.size() << "\n";
 
-    cout <<"o1 == o2: "<< (o1 == o2) << endl;
+    BigDecimalInt test0(n3), test11(n3);
+    BigDecimalInt c = test0 + test11;
+    BigDecimalInt g = test0 - test11;
+    cout << "---------TEST FIVE---------" << "\n";
+    cout << c;
+    cout << g;
+    cout << (test0 < test11) << "\n";
+    cout << (test0 > test11) << "\n";
+    cout << test0.Sign() << "\n";
+    cout << test11.size() << "\n";
 
-    cout <<"o2 > o3: "<< (o2 > o3) << endl;
+    BigDecimalInt test(n3), test2(n2);
+    BigDecimalInt j = test + test2;
+    BigDecimalInt i = test - test2;
+    cout << "---------TEST SIX---------" << "\n";
+    cout << j;
+    cout << i;
+    cout << (test < test2) << "\n";
+    cout << (test > test2) << "\n";
+    cout << test.Sign() << "\n";
+    cout << test2.size() << "\n";
 
-    cout <<"o3 == o4: "<< (o3 == o4) << endl;
-    cout <<"o3 > o4: "<< (o3 > o4) << endl;
+    BigDecimalInt test3(n2), test4(n1);
+    BigDecimalInt o = test3 + test4;
+    BigDecimalInt p = test3 - test4;
+    cout << "---------TEST SEVEN---------" << "\n";
+    cout << o;
+    cout << p;
+    cout << (test3 < test4) << "\n";
+    cout << (test3 > test4) << "\n";
+    cout << test3.Sign() << "\n";
+    cout << test4.size() << "\n";
 
-    cout <<"o3: "<< o3 << endl;
-    cout <<"o3.Size(): "<< o3.Size();
+    BigDecimalInt test5(n3), test6(n4);
+    BigDecimalInt q = test5 + test6;
+    BigDecimalInt w = test5 - test6;
+    cout << "---------TEST EIGHT---------" << "\n";
+    cout << q;
+    cout << w;
+    cout << (test5 < test6) << "\n";
+    cout << (test5 > test6) << "\n";
+    cout << test5.Sign() << "\n";
+    cout << test6.size() << "\n";
+
+    BigDecimalInt test7(n3), test8(n1);
+    BigDecimalInt m = test7 + test8;
+    BigDecimalInt n = test7 - test8;
+    cout << "---------TEST NINE---------" << "\n";
+    cout << m;
+    cout << n;
+    cout << (test7 < test8) << "\n";
+    cout << (test7 > test8) << "\n";
+    cout << test7.Sign() << "\n";
+    cout << test8.size() << "\n";
+
+    BigDecimalInt test9(n2), test10(n4);
+    BigDecimalInt x = test9 + test10;
+    BigDecimalInt y = test9 - test10;
+    cout << "---------TEST TEN---------" << "\n";
+    cout << x;
+    cout << y;
+    cout << (test9 < test10) << "\n";
+    cout << (test9 > test10) << "\n";
+    cout << test9.Sign() << "\n";
+    cout << test10.size() << "\n";*/
+    string s1,s2;
+    while(cin>>s1>>s2){
+        BigDecimalInt a(s1),b(s2);
+        cout<<a+b<<endl;
+        cout<<a-b<<endl;
+        cout<<b-a<<endl;
+        cout << (a < b) << "\n";
+        cout << (a > b) << "\n";
+        cout << a.sign() << "\n";
+        cout << b.size() << "\n";
+        cout << a.getNumber() << "\n";
+        a.setNumber("100");
+        cout << a.getNumber() << "\n" <<"\n";
+    }
+
+    return 0;
 }
