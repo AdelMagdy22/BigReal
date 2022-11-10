200c19a76c2d6b3242e03645b89c620e1c56604c
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-10 15:27:24 +0200

    v1.2

--- Unlimited_Real.cpp
+++ Unlimited_Real.cpp
@@ -5,9 +5,40 @@
 int main()
 {
     BigDecimalInt o3("65");
-    string s1 = "2568.55";
-    BigReal o1(s1), o2("+558.70");
-    // BigReal o5(o3);
-    o1.assNumber("-555.788");
+    string s1 = "+50.65";
+    BigReal o1(s1), o2("+52.40");
+    BigReal o5(o3), o4("55.00");
 
+    cout<<"\no5: "<<o5.returnNumber()<<endl;
+    cout<<"o5 decPointPos: "<<o5.getDecPointPos()<<endl; //65.00
+    
+    cout<<"\n 50.65 + 52.40 = ";
+
+    string re = addition(o1.returnNumber(),o2.returnNumber());
+
+    for (long long i = 0; i < re.length(); i++)
+    {
+        int resDecPointPos; 
+        resDecPointPos = max(o1.getDecPointPos(), o2.getDecPointPos()) + (re.length() - max(o1.returnNumber().length(), o2.returnNumber().length() ));
+        if (i == resDecPointPos)
+        {
+            cout<<'.';
+            cout<<re[i];
+        }
+        else cout<<re[i];
+    }    
+    cout<<endl;
+
+    BigReal o6(move(o4));
+    cout<<o6.returnNumber()<<" DecPointPos: "<<o6.getDecPointPos()<<endl;
+    cout<<o4.returnNumber()<<" DecPointPos: "<<o4.getDecPointPos()<<endl;
+
+    BigReal o7("55.50");
+    BigReal o8;
+    o8 = move(o7);
+    cout<<'\n'<<o8.returnNumber()<<endl;
+
+    o1 = o8;
+    cout<<'\n'<<"o1: "<<o1.returnNumber()<<endl;
+    cout<<'\n'<<"o7: "<<o7.returnNumber()<<endl;  
 }
