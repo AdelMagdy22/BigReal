12e39bf8cafb8dce59d2381eaf1a655e39d2397a
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-10 16:53:54 +0200

    v1.2

--- BigRealClass.cpp
+++ BigRealClass.cpp
@@ -13,10 +13,11 @@ void BigReal :: assNumber(string num)
     bool ValidRealNumber = checkValidInputRealNum(num);
     if(ValidRealNumber)
     {
-        pos = num.find(".",1);
-        cout<<"decPointPos: "<< pos<<endl;
-        decPointPosFromEnd = num.length() - pos;
-        cout<<"decPointPosFromEnd: "<< decPointPosFromEnd<<endl;
+        pos = num.find(".",0);
+
+        if(num[0] == '+' || num[0] == '-') decPointPos = pos - 1;
+        else decPointPos = pos;
+        cout<<"\n"<<"decPointPos: "<< decPointPos<<endl;
         num.erase(pos, 1);
         wholeNum.setNumber(num);
         cout<<wholeNum.sign()<<wholeNum.getNumber()<<endl;
@@ -27,14 +28,49 @@ void BigReal :: assNumber(string num)
         exit(1);
     }    
 }
-BigReal :: BigReal(const BigReal& other)
+
+BigReal :: BigReal(BigDecimalInt other)
 {
+    wholeNum = other.getNumber(); 
+    decPointPos = other.getNumber().length();
+}
+
+BigReal :: BigReal(const BigReal& other) // copy constructor
+{
+    cout<<'\n'<<"copy constructor";
     wholeNum = other.wholeNum;
-    decPointPosFromEnd = other.decPointPosFromEnd;
+    decPointPos = other.decPointPos;
 } 
-/*
+
 BigReal :: BigReal(BigReal&& other)
 {
-    wholeNum = other.wholeNum;
-    // other.wholeNum = nullptr;
-}*/
+    cout<<'\n'<<"move constructor";
+    this->wholeNum = other.wholeNum;
+    this->decPointPos = other.decPointPos;
+    other.assNumber("0.00");    
+}
+
+BigReal& BigReal :: operator = (BigReal& other)
+{
+    cout<<'\n'<<"assingment operator";
+    if(this != &other)
+    {
+        this->wholeNum = other.wholeNum;
+        this->decPointPos = other.decPointPos;
+    }
+
+    return *this;
+}
+
+BigReal& BigReal:: operator = (BigReal&& other)
+{
+    cout<<"\n"<<"move Assignment"<<endl;
+    if(this != &other)
+    {
+        this->wholeNum = other.wholeNum;
+        this->decPointPos = other.decPointPos;
+        other.assNumber("0.00");
+    }
+
+    return *this;
+}
