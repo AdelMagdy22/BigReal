0c7318bfc39c1347cea90fcce2d51a9aac04ec4e
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-10 18:07:35 +0200

    v1.3

--- Unlimited_Real.cpp
+++ Unlimited_Real.cpp
@@ -1,4 +1,19 @@
+// FCAI – Object-Oriented Programming 2 – 2022 - Assignment 2
+// Program Name: BigReal
+// Last Modification Date: 13/11/2022
+// Author1 and ID and Group: Adel Magdy Abd El-Hay Mohammed  ID: 20210190 (Orange Part (First))
+// Author2 and ID and Group: Mohammed Ayman  ID: 20210569 (Black Part (Second))
+// Author3 and ID and Group: Asmaa Saleh ID: 20211014 (Blue Part (last))
+/*
+description: In this problem we developed a new C++ type (class).
+that can hold unlimited Real values and performs arithmetic operations on them.
+such as: +, -, <, and >,
+diffrent things such as: Copy Constructor, move constructor, Assignment operator and move assignment.
+and we use BigDecimalIntClass that BGad do it.
+ */
+
 // #include "BigRealClass.h"
+// #include "BigDecimalIntClass.h"
 #include "BigRealClass.cpp"
 #include "BigDecimalIntClass.cpp"
 
@@ -30,6 +45,9 @@ int main()
     cout<<endl;
 
     BigReal o6(move(o4));
+
+
+    
     cout<<o6.returnNumber()<<" DecPointPos: "<<o6.getDecPointPos()<<endl;
     cout<<o4.returnNumber()<<" DecPointPos: "<<o4.getDecPointPos()<<endl;
 
