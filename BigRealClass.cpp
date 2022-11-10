9a7136838e280c4c0ea7db0b7cdbfdb5a44d408c
Adel Magdy <101670025+AdelMagdy22@users.noreply.github.com>
2022-11-10 18:05:02 +0200

    v1.3

--- BigRealClass.cpp
+++ BigRealClass.cpp
@@ -1,76 +1,58 @@
-#include "BigRealClass.h"
+// FCAI – Object-Oriented Programming 2 – 2022 - Assignment 2
+// Program Name: BigReal
+// Last Modification Date: 10/11/2022
+// Author1 and ID and Group: Adel Magdy Abd El-Hay Mohammed  ID: 20210190 (Orange Part (First))
+// Author2 and ID and Group: Mohammed Ayman  ID: 20210569 (Black Part (Second))
+// Author3 and ID and Group: Asmaa Saleh ID: 20211014 (Blue Part (last))
+/*
+description: In this problem we developed a new C++ type (class).
+that can hold unlimited Real values and performs arithmetic operations on them.
+such as: +, -, <, and >,
+diffrent things such as: Copy Constructor, move constructor, Assignment operator and move assignment.
+and we use BigDecimalIntClass that BGad do it.
+ */
 
-bool BigReal :: checkValidInputRealNum(string input)
-{
-    const regex ValidInput("[+-]?[0-9]+\\.[0-9]+");
-    return regex_match(input, ValidInput);
-    
-}
+#ifndef BigRealClass_H
+#define BigRealClass_H
 
-void BigReal :: assNumber(string num)
-{
-    int pos;
-    bool ValidRealNumber = checkValidInputRealNum(num);
-    if(ValidRealNumber)
+#include "BigDecimalIntClass.h"
+
+class BigReal
+{ 
+private:
+    BigDecimalInt wholeNum;
+    int decPointPos; // position of Decimal Point
+    bool checkValidInputRealNum(string input);
+
+public:
+    BigReal():decPointPos(1)
+    {wholeNum.setNumber("000");};
+
+    void assNumber(string num);
+    BigReal(string realNumber)
     {
-        pos = num.find(".",0);
+        assNumber(realNumber);
+    };
 
-        if(num[0] == '+' || num[0] == '-') decPointPos = pos - 1;
-        else decPointPos = pos;
-        cout<<"\n"<<"decPointPos: "<< decPointPos<<endl;
-        num.erase(pos, 1);
-        wholeNum.setNumber(num);
-        cout<<wholeNum.sign()<<wholeNum.getNumber()<<endl;
-
-    }else
+    string returnNumber()
     {
-        cout<<"Invalid"<<"\n";
-        exit(1);
-    }    
-}
-
-BigReal :: BigReal(BigDecimalInt other)
-{
-    wholeNum = other.getNumber(); 
-    decPointPos = other.getNumber().length();
-}
-
-BigReal :: BigReal(const BigReal& other) // copy constructor
-{
-    cout<<'\n'<<"copy constructor";
-    wholeNum = other.wholeNum;
-    decPointPos = other.decPointPos;
-} 
-
-BigReal :: BigReal(BigReal&& other)
-{
-    cout<<'\n'<<"move constructor";
-    this->wholeNum = other.wholeNum;
-    this->decPointPos = other.decPointPos;
-    other.assNumber("0.00");    
-}
-
-BigReal& BigReal :: operator = (BigReal& other)
-{
-    cout<<'\n'<<"assingment operator";
-    if(this != &other)
+        return wholeNum.getNumber();
+    }
+    int getDecPointPos()
     {
-        this->wholeNum = other.wholeNum;
-        this->decPointPos = other.decPointPos;
+        return decPointPos;
     }
 
-    return *this;
-}
 
-BigReal& BigReal:: operator = (BigReal&& other)
-{
-    cout<<"\n"<<"move Assignment"<<endl;
-    if(this != &other)
-    {
-        this->wholeNum = other.wholeNum;
-        this->decPointPos = other.decPointPos;
-        other.assNumber("0.00");
-    }
+    BigReal(BigDecimalInt bigInt);
 
-    return *this;
-}
+    BigReal (const BigReal& other); // copy constructor
+
+    BigReal& operator= ( BigReal& other); // Assignment operator
+
+    BigReal (BigReal&& other); // move constructor
+
+    BigReal& operator= (BigReal&& other); // move Assignment
+};
+
+#endif
